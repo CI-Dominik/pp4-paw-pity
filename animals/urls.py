a37f235c880d4fac72426dd6fa5bf2dba45b2c6e
Dominik Wulf <Mail@Dominik-Wulf.de>
2024-12-02 20:06:35 +0100

    Add ability to change animal entry

--- animals/urls.py
+++ animals/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('add_animal/', views.add_animal, name='add_animal'),
+    path('delete_animal/<int:animal_id>', views.delete_animal,
+         name='delete_animal'),
+    path('edit_animal/<int:animal_id>', views.edit_animal, name='edit_animal'),
     path('', views.animals, name='animals'),
 ]
--- animals/views.py
+++ animals/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .forms import AnimalForm
 from django.contrib.auth.decorators import login_required
 from .models import Animal
@@ -36,3 +36,30 @@ def add_animal(request):
         form = AnimalForm()
 
     return render(request, 'animals/add_animal.html', {'form': form})
+
+
+@login_required
+def edit_animal(request, animal_id):
+    animal = get_object_or_404(Animal, id=animal_id)
+    success_message = None
+    if request.method == 'POST':
+        form = AnimalForm(request.POST, request.FILES, instance=animal)
+        if form.is_valid():
+            form.save()
+            success_message = f"{animal.name} successfully updated!"
+    else:
+        form = AnimalForm(instance=animal)
+    return render(
+        request,
+        'animals/edit_animal.html',
+        {'form': form, 'success_message': success_message}
+    )
+
+
+@login_required
+def delete_animal(request, animal_id):
+    return render(
+        request,
+        'animals/delete_animal.html',
+        {'animal_id': animal_id}
+    )
