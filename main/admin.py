967311002624424b0a6e22f56b50ac6e54845592
Dominik Wulf <Mail@Dominik-Wulf.de>
2024-12-15 20:26:56 +0100

    Update CustomUser model for admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.sites.models import Site
-from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
-admin.site.register(CustomUser, UserAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {'classes': ('wide',), 'fields': ('username', 'email', 'password1', 'password2')}),
+    )
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.unregister(Group)
 admin.site.unregister(Site)
