5e78d676dde7350fdcc09e5814b2555ecc4b8d9c
Dominik Wulf <Mail@Dominik-Wulf.de>
2024-12-16 03:23:20 +0100

    Change database and complaints design, add paginator

--- complaints/views.py
+++ complaints/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import CommentComplaintForm
 from reports.models import Comment
 from .models import CommentComplaint
+from django.core.paginator import Paginator
 
 
 def complain_comment(request, comment_id):
@@ -23,7 +24,10 @@ def view_complaints(request):
     if not request.user.is_superuser:
         return render(request, '403.html', status=403)
     complaints = CommentComplaint.objects.all()
-    return render(request, 'complaints/complaints_list.html', {'complaints': complaints})
+    paginator = Paginator(complaints, 6)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'complaints/complaints_list.html', {'complaints': page_obj})
 
 
 def remove_comment(request, comment_id):
